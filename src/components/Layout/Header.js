7e71273c74d10fbe85dd50da43f4139e33f228ce
ehdrn3020 <ehdrnsla3020@naver.com>
2023-10-03 12:01:49 +0900

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return <Fragment>
+    <header className={classes.header}>
+      <h1>ReactMeals</h1>
+      <button>Cart</button>
+    </header>
+    <div className={classes['main-image']}>
+      <img src={mealsImage} alt="음식주문 페이지의 메인 이미지"/>
+    </div>
+  </Fragment>;
+};
+
+export default Header;
